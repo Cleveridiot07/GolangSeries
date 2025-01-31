364fcef08120ba92419839ac56570922806adc97
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-01-31 11:13:32 +0530

    functions

---
+++ 16Functions/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Functions in Golang");
+	greeter();
+
+	result := adder(5,6);
+	fmt.Println("Addition is ",result);
+
+	// var arr = []int{1,2,3,4,5};
+
+	result2 := proAdder(5,8,7,6,2,5,4,1);
+	fmt.Println("Procedural addition is ",result2);
+	
+
+
+	// Functions definitions are not allowed in another fuctions
+	// func greeterTwo(){
+	// 	fmt.Println("Hello, Greeter Two!");
+	// }
+	// greeterTwo();
+}
+
+func adder(a int, b int) int {
+    return a + b;
+}
+
+func proAdder(values ...int)int{
+	total := 0
+	for _, value := range values {
+        total += value
+    }
+	return total;
+}
+
+
+func greeter(){
+	fmt.Println("Hello, Golang!");
+}
